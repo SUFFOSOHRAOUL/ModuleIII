8788d0db8e0519dad432d23509197f682dda057b
suffosohraoul <sufforaoul3@gmail.com>
2026-09-15 13:03:53 -0400

    implementing polymorphism

---
+++ src/main/java/org/raoulscode/Animal.java
@@ -0,0 +1,42 @@
+package org.raoulscode;
+
+public class Animal {
+    private String name;
+    public Animal(String name){
+        this.name =name;
+    }
+
+    public String sound(){
+        return null;
+    }
+    public String toString(){
+        return name.concat("says").concat(sound());
+    }
+}
+
+class Dog extends Animal{
+    public Dog(String name){
+        super(name);
+    }
+
+    public String sound(){
+        return "Woof";
+    }
+}
+class Cat extends Animal{
+    public Cat (String name){
+        super(name);
+    }
+    public String sound(){
+        return "Moew";
+    }
+}
+class Cow extends Animal{
+    public Cow (String name){
+        super(name);
+
+    }
+    public String sound(){
+        return "Moo";
+    }
+}
